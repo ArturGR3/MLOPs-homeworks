752e86b51c13c03b79c1c7d3605bbb3aa3d202c5
ArturGR3 <arturgrygorian@gmail.com>
2024-06-18 11:03:05 +0000

    hw5

--- homework5/hw5/baseline_model_nyc_taxi_data.py
+++ homework5/hw5/baseline_model_nyc_taxi_data.py
@@ -1,18 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Baseline model for batch monitoring example
-
-# In[38]:
-
-
 import requests
 import datetime
 import pandas as pd
 
 from evidently import ColumnMapping
 from evidently.report import Report
-from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric
+from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric,  ColumnQuantileMetric, RegressionPerformanceMetrics
 
 from joblib import load, dump
 from tqdm import tqdm
@@ -20,325 +12,76 @@ from tqdm import tqdm
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 
-
-# In[2]:
-
-
-files = [('green_tripdata_2022-02.parquet', './data'), ('green_tripdata_2022-01.parquet', './data')]
-
-print("Download files:")
-for file, path in files:
-    url=f"https://d37ci6vzurychx.cloudfront.net/trip-data/{file}"
-    resp=requests.get(url, stream=True)
-    save_path=f"{path}/{file}"
-    with open(save_path, "wb") as handle:
-        for data in tqdm(resp.iter_content(),
-                        desc=f"{file}",
-                        postfix=f"save to {save_path}",
-                        total=int(resp.headers["Content-Length"])):
-            handle.write(data)
-
-
-# In[3]:
-
-
-jan_data = pd.read_parquet('data/green_tripdata_2022-01.parquet')
-
-
-# In[4]:
-
-
-jan_data.describe()
-
-
-# In[5]:
-
-
-jan_data.shape
-
-
-# In[6]:
-
-
-# create target
-jan_data["duration_min"] = jan_data.lpep_dropoff_datetime - jan_data.lpep_pickup_datetime
-jan_data.duration_min = jan_data.duration_min.apply(lambda td : float(td.total_seconds())/60)
-
-
-# In[7]:
-
-
-# filter out outliers
-jan_data = jan_data[(jan_data.duration_min >= 0) & (jan_data.duration_min <= 60)]
-jan_data = jan_data[(jan_data.passenger_count > 0) & (jan_data.passenger_count <= 8)]
-
-
-# In[28]:
-
-
-jan_data.duration_min.hist()
-
-
-# In[9]:
-
-
-# data labeling
-target = "duration_min"
-num_features = ["passenger_count", "trip_distance", "fare_amount", "total_amount"]
-cat_features = ["PULocationID", "DOLocationID"]
-
-
-# In[10]:
-
-
-jan_data.shape
-
-
-# In[11]:
-
-
-train_data = jan_data[:30000]
-val_data = jan_data[30000:]
-
-
-# In[12]:
-
-
-model = LinearRegression()
-
-
-# In[13]:
-
-
-model.fit(train_data[num_features + cat_features], train_data[target])
-
-
-# In[14]:
-
-
-train_preds = model.predict(train_data[num_features + cat_features])
-train_data['prediction'] = train_preds
-
-
-# In[15]:
-
-
-val_preds = model.predict(val_data[num_features + cat_features])
-val_data['prediction'] = val_preds
-
-
-# In[16]:
-
-
-print(mean_absolute_error(train_data.duration_min, train_data.prediction))
-print(mean_absolute_error(val_data.duration_min, val_data.prediction))
-
-
-# # Dump model and reference data
-
-# In[17]:
-
-
-with open('models/lin_reg.bin', 'wb') as f_out:
-    dump(model, f_out)
-
-
-# In[18]:
-
-
-val_data.to_parquet('data/reference.parquet')
-
-
-# # Evidently Report
-
-# In[19]:
-
-
-column_mapping = ColumnMapping(
-    target=None,
-    prediction='prediction',
-    numerical_features=num_features,
-    categorical_features=cat_features
-)
-
-
-# In[20]:
-
-
-report = Report(metrics=[
-    ColumnDriftMetric(column_name='prediction'),
-    DatasetDriftMetric(),
-    DatasetMissingValuesMetric()
-]
-)
-
-
-# In[21]:
-
-
-report.run(reference_data=train_data, current_data=val_data, column_mapping=column_mapping)
-
-
-# In[22]:
-
-
-report.show(mode='inline')
-
-
-# In[23]:
-
-
-result = report.as_dict()
-
-
-# In[24]:
-
-
-result
-
-
-# In[25]:
-
-
-#prediction drift
-result['metrics'][0]['result']['drift_score']
-
-
-# In[26]:
-
-
-#number of drifted columns
-result['metrics'][1]['result']['number_of_drifted_columns']
-
-
-# In[27]:
-
-
-#share of missing values
-result['metrics'][2]['result']['current']['share_of_missing_values']
-
-
-# # Evidently Dashboard
-
-# In[32]:
-
-
-from evidently.metric_preset import DataDriftPreset, DataQualityPreset
-
-from evidently.ui.workspace import Workspace
-from evidently.ui.dashboards import DashboardPanelCounter, DashboardPanelPlot, CounterAgg, PanelValue, PlotType, ReportFilter
-from evidently.renderers.html_widgets import WidgetSize
-
-
-# In[30]:
-
-
-ws = Workspace("workspace")
-
-
-# In[31]:
-
-
-project = ws.create_project("NYC Taxi Data Quality Project")
-project.description = "My project descriotion"
-project.save()
-
-
-# In[33]:
-
-
-regular_report = Report(
-    metrics=[
-        DataQualityPreset()
-    ],
-    timestamp=datetime.datetime(2022,1,28)
-)
-
-regular_report.run(reference_data=None,
-                  current_data=val_data.loc[val_data.lpep_pickup_datetime.between('2022-01-28', '2022-01-29', inclusive="left")],
-                  column_mapping=column_mapping)
-
-regular_report
-
-
-# In[34]:
-
-
-ws.add_report(project.id, regular_report)
-
-
-# In[35]:
-
-
-#configure the dashboard
-project.dashboard.add_panel(
-    DashboardPanelCounter(
-        filter=ReportFilter(metadata_values={}, tag_values=[]),
-        agg=CounterAgg.NONE,
-        title="NYC taxi data dashboard"
-    )
-)
-
-project.dashboard.add_panel(
-    DashboardPanelPlot(
-        filter=ReportFilter(metadata_values={}, tag_values=[]),
-        title="Inference Count",
-        values=[
-            PanelValue(
-                metric_id="DatasetSummaryMetric",
-                field_path="current.number_of_rows",
-                legend="count"
-            ),
-        ],
-        plot_type=PlotType.BAR,
-        size=WidgetSize.HALF,
-    ),
-)
-
-project.dashboard.add_panel(
-    DashboardPanelPlot(
-        filter=ReportFilter(metadata_values={}, tag_values=[]),
-        title="Number of Missing Values",
-        values=[
-            PanelValue(
-                metric_id="DatasetSummaryMetric",
-                field_path="current.number_of_missing_values",
-                legend="count"
-            ),
-        ],
-        plot_type=PlotType.LINE,
-        size=WidgetSize.HALF,
-    ),
-)
-
-project.save()
-
-
-# In[36]:
-
-
-regular_report = Report(
-    metrics=[
-        DataQualityPreset()
-    ],
-    timestamp=datetime.datetime(2022,1,29)
-)
-
-regular_report.run(reference_data=None,
-                  current_data=val_data.loc[val_data.lpep_pickup_datetime.between('2022-01-29', '2022-01-30', inclusive="left")],
-                  column_mapping=column_mapping)
-
-regular_report
-
-
-# In[37]:
-
-
-ws.add_report(project.id, regular_report)
-
-
-# In[ ]:
-
-
-
+year = 2024
+month = 3
+
+def download_taxi_data(year, month):
+    files = [(f'green_tripdata_{year}-{month}.parquet', './data')]
+
+    for file, path in files:
+        url=f"https://d37ci6vzurychx.cloudfront.net/trip-data/{file}"
+        resp=requests.get(url, stream=True)
+        save_path=f"{path}/{file}"
+        with open(save_path, "wb") as handle:
+            for data in tqdm(resp.iter_content(),
+                            desc=f"{file}",
+                            postfix=f"save to {save_path}",
+                            total=int(resp.headers["Content-Length"])):
+                handle.write(data)
+
+def data_preprocessing(data_path = 'data/green_tripdata_2024-03.parquet'):
+    
+    march_data = pd.read_parquet(data_path)
+    print(f'Nrows: {march_data.shape[0]}')
+    
+    # Filter only March 2024
+    march_data = march_data[(march_data.lpep_pickup_datetime >= '2024-03-01') & (march_data.lpep_pickup_datetime < '2024-04-01')]
+    
+    # Create a column that finds daily median (lpep_pickup_datetime) for fare_amount
+    march_data['median_fare_amount'] = march_data.groupby(march_data.lpep_pickup_datetime.dt.date).fare_amount.transform('median')
+    
+    # Find the maximum median fare amount
+    print(f"Max daily median for fare amount is: {march_data.median_fare_amount.max():.2f}") 
+    
+    # create target
+    march_data["duration_min"] = march_data.lpep_dropoff_datetime - march_data.lpep_pickup_datetime
+    march_data.duration_min = march_data.duration_min.apply(lambda td : float(td.total_seconds())/60)
+    
+    # filter out outliers
+    march_data = march_data[(march_data.duration_min >= 0) & (march_data.duration_min <= 60)]
+    march_data = march_data[(march_data.passenger_count > 0) & (march_data.passenger_count <= 8)]
+
+    # data labeling
+    target = "duration_min"
+    num_features = ["passenger_count", "trip_distance", "fare_amount", "total_amount"]
+    cat_features = ["PULocationID", "DOLocationID"]
+
+    train_data = march_data[:30000]
+    val_data = march_data[30000:]
+    
+    return train_data, val_data, target, num_features, cat_features
+
+train_data, val_data, target, num_features, cat_features = data_preprocessing()
+
+def model_building(train_data, val_data, target, num_features, cat_features):
+    
+    # train model
+    model = LinearRegression().fit(train_data[num_features + cat_features], train_data[target])
+
+    # evaluate model
+    train_preds = model.predict(train_data[num_features + cat_features])
+    train_data['prediction'] = train_preds
+
+    val_preds = model.predict(val_data[num_features + cat_features])
+    val_data['prediction'] = val_preds
+
+    # Find the mean absolute error for train and validation data
+    print(mean_absolute_error(train_data.duration_min, train_data.prediction))
+    print(mean_absolute_error(val_data.duration_min, val_data.prediction))
+
+    # Save the model
+    with open('models/lin_reg.bin', 'wb') as f_out:
+        dump(model, f_out)
+
+    val_data.to_parquet('data/reference.parquet')
 
