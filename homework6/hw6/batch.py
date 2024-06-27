50d2ceb2083053738a2562f940487119ff9d5909
ArturGR3 <arturgrygorian@gmail.com>
2024-06-27 12:08:10 +0000

    chore: Add Makefile for quality checks, tests, and integration tests

--- homework6/hw6/batch.py
+++ homework6/hw6/batch.py
@@ -4,70 +4,81 @@
 import os
 import sys
 import pickle
-import pandas as pd
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv(usecwd=True))    
+import pandas as pd
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv(usecwd=True))
+
 
 def prepare_data(data_frame, categorical_columns):
     """
     Prepares data by calculating duration and filtering based on it.
     """
-    categorical_columns = ['PULocationID', 'DOLocationID']
-    
-    data_frame['duration'] = data_frame.tpep_dropoff_datetime - data_frame.tpep_pickup_datetime
-    data_frame['duration'] = data_frame.duration.dt.total_seconds() / 60
+    categorical_columns = ["PULocationID", "DOLocationID"]
+
+    data_frame["duration"] = data_frame.tpep_dropoff_datetime - data_frame.tpep_pickup_datetime
+    data_frame["duration"] = data_frame.duration.dt.total_seconds() / 60
 
     data_frame = data_frame[(data_frame.duration >= 1) & (data_frame.duration <= 60)].copy()
 
-    data_frame[categorical_columns] = data_frame[categorical_columns].fillna(-1).astype('int').astype('str')    
+    data_frame[categorical_columns] = (
+        data_frame[categorical_columns].fillna(-1).astype("int").astype("str")
+    )
     return data_frame
+
 
 def read_data(filename, categorical_columns):
     """
     Reads data from a file, supporting reading from S3 if configured.
     """
-    s3_endpoint_url = os.getenv('S3_ENDPOINT_URL')
-    
+    s3_endpoint_url = os.getenv("S3_ENDPOINT_URL")
+
     if s3_endpoint_url is not None:
-        options = {'client_kwargs': {'endpoint_url': s3_endpoint_url}}
+        options = {"client_kwargs": {"endpoint_url": s3_endpoint_url}}
         data_frame = pd.read_parquet(filename, storage_options=options)
-        print('read data from s3')
+        print("read data from s3")
     else:
         data_frame = pd.read_parquet(filename)
-        print('read data from local')
+        print("read data from local")
 
     return prepare_data(data_frame, categorical_columns)
+
 
 def save_data(data_frame, filename):
     """
     Saves data to a file, supporting saving to S3 if configured.
     """
-    s3_endpoint_url = os.getenv('S3_ENDPOINT_URL')
-    
+    s3_endpoint_url = os.getenv("S3_ENDPOINT_URL")
+
     if s3_endpoint_url is not None:
-        options = {'client_kwargs': {'endpoint_url': s3_endpoint_url}}
-        data_frame.to_parquet(filename, engine='pyarrow', index=False, storage_options=options)
-        print('saved data to s3')
+        options = {"client_kwargs": {"endpoint_url": s3_endpoint_url}}
+        data_frame.to_parquet(filename, engine="pyarrow", index=False, storage_options=options)
+        print("saved data to s3")
     else:
-        data_frame.to_parquet(filename, engine='pyarrow', index=False)
-        print('saved data to local')
+        data_frame.to_parquet(filename, engine="pyarrow", index=False)
+        print("saved data to local")
+
 
 def get_input_path(year, month):
     """
     Generates the input file path based on the year and month.
     """
-    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    default_input_pattern = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet"
+    input_pattern = os.getenv("INPUT_FILE_PATTERN", default_input_pattern)
     return input_pattern.format(year=year, month=month)
+
 
 def get_output_path(year, month):
     """
     Generates the output file path based on the year and month.
     """
-    default_output_pattern = 's3://nyc-duration/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet'
-    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    default_output_pattern = (
+        "s3://nyc-duration/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet"
+    )
+    output_pattern = os.getenv("OUTPUT_FILE_PATTERN", default_output_pattern)
     return output_pattern.format(year=year, month=month)
+
 
 def main(year, month):
     """
@@ -75,31 +86,32 @@ def main(year, month):
     """
     input_file = get_input_path(year, month)
     output_file = get_output_path(year, month)
-    
-    # load the model 
-    with open('model.bin', 'rb') as f_in:
+
+    # load the model
+    with open("model.bin", "rb") as f_in:
         dv, lr = pickle.load(f_in)
 
-    # prepare the data 
-    categorical_columns = ['PULocationID', 'DOLocationID']
+    # prepare the data
+    categorical_columns = ["PULocationID", "DOLocationID"]
 
     data_frame = read_data(input_file, categorical_columns)
-    data_frame['ride_id'] = f'{year:04d}/{month:02d}_' + data_frame.index.astype('str')
+    data_frame["ride_id"] = f"{year:04d}/{month:02d}_" + data_frame.index.astype("str")
 
-    dicts = data_frame[categorical_columns].to_dict(orient='records')
+    dicts = data_frame[categorical_columns].to_dict(orient="records")
     x_val = dv.transform(dicts)
     y_pred = lr.predict(x_val)
 
-    print('predicted mean duration:', y_pred.mean().round(2))
+    print("predicted mean duration:", y_pred.mean().round(2))
 
     df_result = pd.DataFrame()
-    df_result['ride_id'] = data_frame['ride_id']
-    df_result['predicted_duration'] = y_pred
-        
+    df_result["ride_id"] = data_frame["ride_id"]
+    df_result["predicted_duration"] = y_pred
+
     save_data(df_result, output_file)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     year = int(sys.argv[1])
     month = int(sys.argv[2])
-    
+
     main(year=year, month=month)
--- homework6/hw6/tests/test_batch.py
+++ homework6/hw6/tests/test_batch.py
@@ -1,37 +1,53 @@
+# pylint: disable=import-error
 
-from datetime import datetime
-import pandas as pd 
+import os
+
 # Add the directory containing batch.py to the Python path
 import sys
-import os
+from datetime import datetime
+
+import pandas as pd
 from deepdiff import DeepDiff
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))                
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from batch import prepare_data
+
 
 def dt(hour, minute, second=0):
     return datetime(2023, 1, 1, hour, minute, second)
 
+
 def test_prepare_data():
-    
+    # pylint: disable=duplicate-code
     data = [
         (None, None, dt(1, 1), dt(1, 10)),
         (1, 1, dt(1, 2), dt(1, 10)),
         (1, None, dt(1, 2, 0), dt(1, 2, 59)),
-        (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
+        (3, 4, dt(1, 2, 0), dt(2, 2, 1)),
     ]
 
-    columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+    columns = [
+        "PULocationID",
+        "DOLocationID",
+        "tpep_pickup_datetime",
+        "tpep_dropoff_datetime",
+    ]
     df = pd.DataFrame(data, columns=columns)
-    
-    actual_result = prepare_data(df, categorical_columns=['PULocationID', 'DOLocationID']) 
-    # trim duration to 1 decimal 
-    actual_result['duration'] = actual_result['duration'].round(1)
-    actuals_result_dict = actual_result[['PULocationID', 'DOLocationID', 'duration']].to_dict(orient='records')
-    
-    expected_result_dict = [{'PULocationID': '-1', 'DOLocationID': '-1', 'duration': 9.0},
-    {'PULocationID': '1', 'DOLocationID': '1', 'duration': 8.0}]
-        
+
+    actual_result = prepare_data(df, categorical_columns=["PULocationID", "DOLocationID"])
+    # trim duration to 1 decimal
+    actual_result["duration"] = actual_result["duration"].round(1)
+    actuals_result_dict = actual_result[["PULocationID", "DOLocationID", "duration"]].to_dict(
+        orient="records"
+    )
+
+    expected_result_dict = [
+        {"PULocationID": "-1", "DOLocationID": "-1", "duration": 9.0},
+        {"PULocationID": "1", "DOLocationID": "1", "duration": 8.0},
+    ]
+
     diff = DeepDiff(actuals_result_dict, expected_result_dict, significant_digits=1)
     # print(diff)
-    assert 'values_changed' not in diff
-    
+    assert "values_changed" not in diff
+
+    print('hui')
